544074d6eb93c9d625f096b6db95e1a6f65ea920
OldsoulPhil <brenenshea@gmail.com>
2024-09-13 16:43:34 -0400

    First commit

--- closest.ts
+++ closest.ts
@@ -1,6 +1,25 @@
 export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
+  nums.sort((a, b) => a - b);
+  let n = nums.length;
+  let closestSum = nums[0] + nums[1] + nums[n - 1];
+  for (let i = 0; i < n - 2; i++) {
+    let left = i + 1;
+    let right = n - 1;
+    while (left < right) {
+      let currentSum = nums[i] + nums[left] + nums[right];
+      if (currentSum === target) {
+        return target;
+      } else if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+    }
+  }
+  return closestSum;
 }
 
-module.exports = { closest }
+module.exports = { closest };
